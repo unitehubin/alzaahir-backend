76cd91315829e146d4888cb8e4720a5cf6c0d24e
majid-nope <muhammed0000majid@gmail.com>
2025-01-29 12:09:16 +0530

    modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
 import { CommonModule } from './common/common.modules';
-import { TestModule } from './modules/test/test.module';
+
 
 @Module({
-  imports: [CommonModule, TestModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [CommonModule, ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
