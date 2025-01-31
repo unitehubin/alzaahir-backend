e55d3f9805dc61c814bb13c006675666e5f56b2a
dheeraj-deb <dheerajt9539@gmail.com>
2025-01-31 23:58:56 +0530

    updated prisma.bat

--- scripts/prisma.bat
+++ scripts/prisma.bat
@@ -15,7 +15,7 @@ set "ENV_FILE=.\env\%1.env"
 echo !ENV_FILE!
 
 :: Check if environment file exists
-if not exist "%ENV_FILE%" (
+if not exist %ENV_FILE% (
     echo Environment file %ENV_FILE% not found.
     exit /b 1
 )
@@ -35,7 +35,7 @@ for /f "usebackq delims=" %%G in ("%ENV_FILE%") do (
 )
 
 :: Set database URL
-set "DATABASE_URL=postgresql://%MYSQL_USER%:%MYSQL_PASSWORD%@%DB_HOST%/%DB_NAME%?sslmode=require"
+set "DATABASE_URL=postgresql://!MYSQL_USER!:!MYSQL_PASSWORD!@!DB_HOST!/!DB_NAME!?sslmode=require"
 set "DATABASE_URL=!DATABASE_URL!"
 echo !DATABASE_URL!
 
